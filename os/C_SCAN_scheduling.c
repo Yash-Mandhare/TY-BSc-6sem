46dddaa8017323ce1b41a17fc68e58e1c2be4841
Yash-Mandhare <90199334+Yash-Mandhare@users.noreply.github.com>
2024-04-10 16:55:52 +0530

    Update C_SCAN_scheduling.c

--- os/C_SCAN_scheduling.c
+++ os/C_SCAN_scheduling.c
@@ -4,81 +4,93 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void cscan(int request[], int n, int head, char direction) {
+#define MAX_REQUESTS 100
+
+// Function to sort the request array in ascending order
+void sortRequests(int request[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (request[j] > request[j + 1]) {
+                int temp = request[j];
+                request[j] = request[j + 1];
+                request[j + 1] = temp;
+            }
+        }
+    }
+}
+
+// Function to implement C-SCAN algorithm
+int cscan(int request[], int n, int head, char direction) {
     int seek_count = 0;
-    int i, j;
-    int min = 0, max = 199;
-    int range = 200;
-    int visited[n];
+    int current_track = head;
+    int index;
 
-    for (i = 0; i < n; i++)
-        visited[i] = 0;
+    // Sort the requests and find the index where the head is located
+    sortRequests(request, n);
+    for (int i = 0; i < n; i++) {
+        if (request[i] >= head) {
+            index = i;
+            break;
+        }
+    }
 
+    // C-SCAN towards higher tracks
     if (direction == 'r') {
-        printf("Seek Sequence\n%d ", head);
-
-        // Move towards the right until reaching the end
-        for (i = head; i <= max; i++) {
-            for (j = 0; j < n; j++) {
-                if (request[j] == i && !visited[j]) {
-                    printf("%d ", request[j]);
-                    visited[j] = 1;
-                    seek_count += abs(head - request[j]);
-                    head = request[j];
-                }
-            }
+        printf("Seek Sequence\n%d ", current_track);
+        for (int i = index; i < n; i++) {
+            printf("%d ", request[i]);
+            seek_count += abs(current_track - request[i]);
+            current_track = request[i];
         }
+        printf("199 "); // Move to the highest track
+        seek_count += 199 - current_track; 
 
-        // Move from the beginning to the minimum
-        for (i = min; i < head; i++) {
-            for (j = 0; j < n; j++) {
-                if (request[j] == i && !visited[j]) {
-                    printf("%d ", request[j]);
-                    visited[j] = 1;
-                    seek_count += abs(head - request[j]);
-                    head = request[j];
-                }
-            }
+        // Move to the lowest track
+        printf("0 ");
+        seek_count += 199; 
+
+        // C-SCAN towards lower tracks
+        for (int i = 0; i < index; i++) {
+            printf("%d ", request[i]);
+            seek_count += abs(current_track - request[i]);
+            current_track = request[i];
         }
-    } else if (direction == 'l') {
-        printf("Seek Sequence\n%d ", head);
-
-        // Move towards the left until reaching the beginning
-        for (i = head; i >= min; i--) {
-            for (j = 0; j < n; j++) {
-                if (request[j] == i && !visited[j]) {
-                    printf("%d ", request[j]);
-                    visited[j] = 1;
-                    seek_count += abs(head - request[j]);
-                    head = request[j];
-                }
-            }
+    } else { // C-SCAN towards lower tracks
+        printf("Seek Sequence\n%d ", current_track);
+        seek_count += current_track;
+        for (int i = index-1; i >= 0; i--) {
+            printf("%d ", request[i]);
+            seek_count += abs(current_track - request[i]);
+            current_track = request[i];
         }
+        printf("0 "); // Move to the lowest track
+        seek_count += current_track; 
 
-        // Move from the end to the maximum
-        for (i = max; i > head; i--) {
-            for (j = 0; j < n; j++) {
-                if (request[j] == i && !visited[j]) {
-                    printf("%d ", request[j]);
-                    visited[j] = 1;
-                    seek_count += abs(head - request[j]);
-                    head = request[j];
-                }
-            }
+        // Move to the highest track
+        printf("199 ");
+        seek_count += 199 - current_track; 
+
+        // C-SCAN towards higher tracks
+        for (int i = n-1; i >= index; i--) {
+            printf("%d ", request[i]);
+            seek_count += abs(current_track - request[i]);
+            current_track = request[i];
         }
     }
 
     printf("\nTotal Number of head movement : %d\n", seek_count);
+
+    return seek_count;
 }
 
 int main() {
-    int n, head;
+    int n, head, seek_count;
     char direction;
 
-    printf("Enter number of disk requests (up to 100): ");
+    printf("Enter number of disk requests (up to %d): ", MAX_REQUESTS);
     scanf("%d", &n);
 
-    int request[n];
+    int request[MAX_REQUESTS];
     printf("Enter disk request string: ");
     for (int i = 0; i < n; i++)
         scanf("%d", &request[i]);
@@ -89,7 +101,24 @@ int main() {
     printf("Enter direction of movement (l for left, r for right): ");
     scanf(" %c", &direction);
 
-    cscan(request, n, head, direction);
+    seek_count = cscan(request, n, head, direction);
 
     return 0;
 }
+
+
+// Enter number of disk requests (up to 100): 8
+// Enter disk request string: 40 60 20 10 100 90 80 30
+// Enter current head position: 50
+// Enter direction of movement (l for left, r for right): r
+// Seek Sequence
+// 50 60 80 90 100 199 0 10 20 30 40 
+// Total Number of head movement : 468
+
+// Enter number of disk requests (up to 100): 8
+// Enter disk request string: 40 60 20 10 100 90 80 30
+// Enter current head position: 50
+// Enter direction of movement (l for left, r for right): l
+// Seek Sequence
+// 50 40 30 20 10 0 199 100 90 80 60
+// Total Number of head movement : 419
